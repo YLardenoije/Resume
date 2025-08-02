895a00f87ca4b508905f0f50c4fc6731dad26e2a
YLardenoije <yveslardenoije@gmail.com>
2025-08-02 20:01:07 +0200

    fix runnability

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-import Portfolio.SECRETKEY as sk
-SECRET_KEY = sk.get_secret_key()
+# import Portfolio.SECRETKEY as sk
+SECRET_KEY = 90909090909090
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
